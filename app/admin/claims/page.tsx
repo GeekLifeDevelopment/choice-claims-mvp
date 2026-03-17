e74c290d68379b25f5155ed582f32ff8240d99d5
Jason Sorenson <jasonsorenson@Jasons-MBP.wi.rr.com>
2026-03-17 14:28:40 -0500

    ficing build

--- app/admin/claims/page.tsx
+++ app/admin/claims/page.tsx
@@ -1,5 +1,7 @@
 import { prisma } from '../../../lib/prisma'
 
+export const dynamic = 'force-dynamic'
+
 function formatDate(value: Date): string {
   return value.toISOString().replace('T', ' ').slice(0, 16)
 }
